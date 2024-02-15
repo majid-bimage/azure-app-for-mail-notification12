b359adb368eaa3a3b0e068e6dd14a3c09c8dc988
majidevelop <majidtechie@gmail.com>
2024-02-15 14:22:37 +0530

    changed the driver in pyodbc

--- SendMail/__init__.py
+++ SendMail/__init__.py
@@ -30,7 +30,7 @@ app.config['server'] = "bimageforge.database.windows.net"
 app.config['database'] = "bimageforge"
 app.config['username'] = "forge"
 app.config['password'] = "BimageNow2020"
-app.config['driver'] = 'SQL Server'
+app.config['driver'] = 'ODBC Driver 17 for SQL Server'
 
 # Create a connection string
 app.config['conn_str'] = f"DRIVER={{{app.config['driver']}}};SERVER={app.config['server']};DATABASE={app.config['database']};UID={app.config['username']};PWD={app.config['password']}"
